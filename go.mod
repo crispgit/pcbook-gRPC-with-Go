30da0680e53c3bfc7ab16192aa11fe879a7baea5
crispgit <zhoupj1215@hotmail.com>
2021-08-28 13:34:55 -0700

    implement server and in-memory storage

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.15
 require (
 	github.com/golang/protobuf v1.5.0
 	github.com/google/uuid v1.3.0
+	github.com/jinzhu/copier v0.3.2
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
+	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ service/laptop_store.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/jinzhu/copier"
+	"github.com/crispgit/pcbook/pb"
+)
+
+// ErrAlreadyExists is returned when a record with the same ID already exists in the store
+var ErrAlreadyExists = errors.New("record already exists")
+
+// Laptop store is the interface to store laptop
+type LaptopStore interface {
+	// Save the laptop to the store
+	Save (laptop *pb.Laptop) error
+}
+
+// Store laptops in memory
+type InMemoryLaptopStore struct {
+	// read write mutex to handle concurrency
+	mutex sync.RWMutex
+	// laptop.id -> laptop object
+	data map[string]*pb.Laptop
+}
+
+// returns a new InMemoryLaptopStore
+func NewInMemoryLaptopStore() *InMemoryLaptopStore {
+	return &InMemoryLaptopStore{
+		data: make(map[string]*pb.Laptop),
+	}
+}
+
+// Saves the laptop to the store
+func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
+	// require a write lock before adding new object
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.data[laptop.Id] != nil {
+		return ErrAlreadyExists
+	}
+
+	other, err := deepCopy(laptop)
+	if err != nil {
+		return err
+	}
+
+	store.data[other.Id] = other
+	return nil
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
+	other := &pb.Laptop{}
+
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+
+	return other, nil
+}
+
+
+/* Implement the db store in the future
+type DBLaptopStore struct {
+
+}
+*/
