c21bf3a4c0503bacdb9aee422e89a16fcad02536
crispgit <zhoupj1215@hotmail.com>
2021-08-28 21:32:32 -0700

    add search filter proto and store schema

--- service/laptop_store.go
+++ service/laptop_store.go
@@ -1,10 +1,10 @@
 package service
 
 import (
-	// "context"
+	"context"
 	"errors"
 	"fmt"
-	// "log"
+	"log"
 	"sync"
 
 	"github.com/jinzhu/copier"
@@ -20,6 +20,8 @@ type LaptopStore interface {
 	Save (laptop *pb.Laptop) error
 	// Find a laptop by id
 	Find (id string) (*pb.Laptop, error)
+	// Search for laptops with filter, return one by one with func "found"
+	Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 // Store laptops in memory
@@ -69,6 +71,82 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 	return deepCopy(laptop)
 }
 
+// Search searches for laptops with filter, returns one by one via the found function
+func (store *InMemoryLaptopStore) Search(
+	ctx context.Context,
+	filter *pb.Filter,
+	found func(laptop *pb.Laptop) error,
+) error {
+	// require read lock
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		if ctx.Err() == context.Canceled || ctx.Err() == context.DeadlineExceeded {
+			log.Print("context is cancelled")
+			return nil
+		}
+
+		// time.Sleep(time.Second)
+		// log.Print("checking laptop id: ", laptop.GetId())
+
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.GetMaxPriceUsd() {
+		return false
+	}
+
+	if laptop.GetCpu().GetNumberCores() < filter.GetMinCpuCores() {
+		return false
+	}
+
+	if laptop.GetCpu().GetMinGhz() < filter.GetMinCpuGhz() {
+		return false
+	}
+
+	// use to bit because of different ram unit
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return value
+	case pb.Memory_BYTE:
+		return value << 3
+	case pb.Memory_KILOBYTE:
+		return value << 13 // 1024 * 8 = 2^10 * 2^3 = 2^13
+	case pb.Memory_MEGABYTE:
+		return value << 23
+	case pb.Memory_GIGABYTE:
+		return value << 33
+	case pb.Memory_TERABYTE:
+		return value << 43
+	default:
+		return 0
+	}
+}
 
 func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 	other := &pb.Laptop{}
